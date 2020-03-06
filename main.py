ebe699fe8d928772fffceff74bee1bd9d20a6394
adrinvor <adrinvor@qq.com>
2020-03-06 22:44:35 +0800

    git push

--- main.py
+++ main.py
@@ -1,11 +1,10 @@
-import os
-import random
-import sys
-
-import pygame
 from PyQt5.QtCore import *
 from PyQt5.QtGui import *
 from PyQt5.QtWidgets import *
+import sys
+import os
+import random
+import pygame
 
 class Top(QWidget):
     def __init__(self,parent=None):
@@ -17,3 +16,7 @@ Display = TopWindow()
 Display.setFixedSize(900,600)
 Display.show()
 sys.exit(app.exec_())
+
+self.setWindowTitle("消灭病毒小游戏")
+self.setWindowIcon(QIcon("图标.jpg"))
+
